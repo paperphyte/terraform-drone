342081495d75a4565555731dbbd9531c5e8881e3
Jonas Odencrants <krusipo@users.noreply.github.com>
2019-02-25 15:39:02 +0100

    chore: cleanup file structure

--- main.tf
+++ main.tf
@@ -9,6 +9,59 @@ resource "aws_key_pair" "ci_tool" {
   public_key = "${var.ci_tool_pubkey}"
 }
 
+resource "aws_vpc" "ci" {
+  cidr_block           = "172.35.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = "${map("Name", "${var.ci_sub_domain}.${var.root_domain}")}"
+}
+
+resource "aws_subnet" "ci_subnet_a" {
+  vpc_id                  = "${aws_vpc.ci.id}"
+  cidr_block              = "172.35.16.0/20"
+  map_public_ip_on_launch = true
+  availability_zone       = "${var.aws_region}a"
+
+  tags = "${map("Name", "${var.ci_sub_domain}.${var.root_domain}")}"
+}
+
+resource "aws_subnet" "ci_subnet_c" {
+  vpc_id                  = "${aws_vpc.ci.id}"
+  cidr_block              = "172.35.32.0/20"
+  map_public_ip_on_launch = true
+  availability_zone       = "${var.aws_region}c"
+
+  tags = "${map("Name", "${var.ci_sub_domain}.${var.root_domain}")}"
+}
+
+resource "aws_internet_gateway" "ci" {
+  vpc_id = "${aws_vpc.ci.id}"
+
+  tags = "${map("Name", "${var.ci_sub_domain}.${var.root_domain}")}"
+}
+
+resource "aws_route_table" "ci" {
+  vpc_id = "${aws_vpc.ci.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.ci.id}"
+  }
+
+  tags = "${map("Name", "${var.ci_sub_domain}.${var.root_domain}")}"
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = "${aws_subnet.ci_subnet_a.id}"
+  route_table_id = "${aws_route_table.ci.id}"
+}
+
+resource "aws_route_table_association" "c" {
+  subnet_id      = "${aws_subnet.ci_subnet_c.id}"
+  route_table_id = "${aws_route_table.ci.id}"
+}
+
 module "ci_db" {
   source                          = "./modules/database"
   db_identifier                   = "${var.ci_db_identifier}"
