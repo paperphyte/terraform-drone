4890bb990342f9a77e308f4723193d321a07c3c4
Jonas <krusipo@users.noreply.github.com>
2021-10-17 12:18:12 +0200

    docs: add variable description and fix cidr error

--- variables.tf
+++ variables.tf
@@ -1,36 +1,38 @@
 variable "dns_root_name" {
-  default = "paperphyte.com"
-  type    = string
+  type        = string
+  description = "The root domain name such as example.com"
 }
 
 variable "dns_root_id" {
-  default = "kalle"
-  type    = string
+  type        = string
+  description = "The root domain zone name"
 }
 
 variable "drone_admin" {
-  type    = string
-  default = ""
+  type        = string
+  description =  "A default user to be admin of drone at creation"
 }
 
 variable "drone_user_filter" {
-  type    = string
-  default = ""
+  type        = string
+  description = "Organisation or username to allow build from"
 }
 
 variable "allowed_cidr" {
-  type    = list(string)
-  default = ["0.0.0.0/32"]
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+  description = "A cidr to limit access to drone user interface"
 }
 
 variable "versions" {
-  type = object({
+  description = "Version of drone task container image"
+  type        = object({
     server  = string
     secrets = string
     yaml    = string
     runner  = string
   })
-  default = {
+  default     = {
     server  = "v2.4.0"
     secrets = "v1.0.0"
     yaml    = "v0.4.2"
